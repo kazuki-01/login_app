8c2e43f6a17e5b09be35d3d8658faa085d2a553b
Kazuki.M <kazuki0922m@gmail.com>
2022-11-16 23:44:42 +0900

    パラメーターの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters #(設定_許可された_パラメーター)
+    decise_parameter_sanitizer.parmit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.parmit(:sign_in, keys: [:email])
+  end
+
 end
