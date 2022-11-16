1771e117ae309066e4bb83ac878d13cbe42a40cc
Kazuki.M <kazuki0922m@gmail.com>
2022-11-17 00:42:30 +0900

    ルーティング設定
    deviseのデフォルトのコントローラーを参照してしまうので、
    新しくコントローラーを追加したコントローラーを参照するよう
    ルーティング設定をする必要がある！

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    # ↓ローカルに追加されたコントローラーを参照する(コントローラー名: "コントローラーの参照先")
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
